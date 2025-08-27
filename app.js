b52940ec6bc0746b39a65ec8fe65baa91d6f4118
Chiragahuja7 <139047500+Chiragahuja7@users.noreply.github.com>
2025-08-27 18:02:12 +0530

    27th aug

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const cookieParser=require("cookie-parser");
 const session=require("express-session");
 const MongoStore=require("connect-mongo");
 const {isActiveRoute}=require("./server/helpers/routeHelpers")
+const multer = require("multer");
+const path = require("path");
+const Post = require("./server/models/post");
+
 
 const app = express();
 const PORT = 5000 || process.env.PORT;
@@ -19,6 +23,42 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(methodOverride('_method'));
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/"); // folder to save images
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname)); // unique name
+  }
+});
+
+const upload = multer({ storage: storage });
+
+app.use('/uploads', express.static('uploads'));
+
+app.get("/add-blog",(req,res)=>{
+    res.render("form.ejs");
+})
+
+
+app.post("/add-blog", upload.single("image"), async (req, res) => {
+  try {
+    const { title, content } = req.body;
+    const imagePath = req.file ? "/uploads/" + req.file.filename : null;
+
+    const newBlog = new Post({
+      title,
+      body:content,
+      image: imagePath   // ✅ store image path
+    });
+
+    await newBlog.save();
+    res.redirect("/");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error saving blog");
+  }
+});
 
 app.use(session({
     secret:'keyboard cat',
